cf1f9c287ff92a00b1cb687f8b7aac8915eacbc4
tkavlearning <45487677+tkavlearning@users.noreply.github.com>
2024-03-15 15:15:26 +0000

    2 teas, 2 cokes and a pack of biccies plz

--- src/questionGenerator.js
+++ src/questionGenerator.js
@@ -1,15 +1,14 @@
 const questions = [
-  'What is your go-to meal when you have to cook something quick?',
   'What is the last movie you watched?',
   'If you could only have one sandwich for the rest of your life, what would it be?',
   'What is your favorite ice cream flavor? Do you prefer a cone or a cup?',
-  'What is your favorite fast food place?',
   'What is something interesting about where you grew up?',
   'What is your favourite podcast?',
   'You can only have one, would you rather have a cook or a maid?',
   'What is your favourite kitchen appliance?',
   'Irish Lottery - would you rather win 3 million or the 20k a month prize for 30 years?',
   'What is your favorite cereal?',
+  'What song lyrics best represent you?',
   'Whats a Movie you can watch on repeat?',
   'Top 3 ice cream flavors?',
   'Type of music you were raised on',
@@ -19,7 +18,12 @@ const questions = [
   'What is your favorite childhood TV show or cartoon?',
   'Death Row last meal - any meal, any restaurant in the world',
   'Guilty TV or Movie pleasure, share with the class?',
-  'What is your favourite month?'
+  'What is your favourite month?',
+  'Which Muppet represents you the best?',
+  'Its the 90s, which Spice Girl are you taking a bullet for?',
+  'Would you rather, give up Coffee or give up Pizza?',
+
+
 ];
 
 const used = [
@@ -28,6 +32,7 @@ const used = [
   'What is your go-to breakfast? Is it sweet or savory?',
   'Where would you like to go on your next trip?',
   'What is your most-used emoji?',
+  'What is your go-to meal when you have to cook something quick?',
 ];
 
 function getRandomQuestion() {
