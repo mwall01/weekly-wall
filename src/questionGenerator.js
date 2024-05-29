8b4b9d8dfc663bcb61a2220fd97107cd765770f8
William Daly <wdaly@squarespace.com>
2024-05-29 09:57:26 +0100

    Commenting out old questions and adding a new one

--- src/questionGenerator.js
+++ src/questionGenerator.js
@@ -3,21 +3,21 @@ const questions = [
   'What is your favourite podcast?',
   'You can only have one, would you rather have a cook or a maid?',
   'What is your favourite kitchen appliance?',
-  'Irish Lottery - would you rather win 3 million or the 20k a month prize for 30 years?',
   'What song lyrics best represent you?',
   'Whats a Movie you can watch on repeat?',
   'Top 3 ice cream flavors?',
   'Type of music you were raised on',
-  'Would you rather live in a place where it is always hot or always cold?',
   'Would you rather explore space or the ocean?',
   'What is your favorite app on your phone?',
   'What is your favorite childhood TV show or cartoon?',
   'Death Row last meal - any meal, any restaurant in the world',
-  'What is your favourite month?',
-  
+  'What is your favourite IDE?',
 ];
 
 // const used = [
+//   'Would you rather live in a place where it is always hot or always cold?',
+//  'Irish Lottery - would you rather win 3 million or the 20k a month prize for 30 years?',
+//  'What is your favourite month?',
 //   'Window seat or aisle seat?',
 //   'Which movie do you think has the best soundtrack?',
 //   'What is your go-to breakfast? Is it sweet or savory?',
