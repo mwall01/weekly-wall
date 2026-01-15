1e825b0ecd416d8e2d3c8eaa794d7d01dc1af651
Michael Wall <48205225+mwall01@users.noreply.github.com>
2026-01-15 10:47:59 +0000

    Louise creep

--- questionGenerator.js
+++ questionGenerator.js
@@ -1,5 +1,4 @@
 const questions = [
-  'Do you ever nap?',
   'You have to pick one other person on this call to be on time for a meeting with you next week. If they are on time, you get EU10,000. If they are late you owe them EU10,000. Who do you pick?',
   'What extras would you have built into your bathroom mirror? (eg bluetooth speaker, defogger, lights, etc)',
 ];
